dfae88bbe4fb5cf90e42c39a02353600ff990c45
chuchu <eugene.omar@andela.com>
2017-03-16 12:34:41 +0300

    Fix settings.py to only include postgres settings

--- hc/settings.py
+++ hc/settings.py
@@ -90,15 +90,15 @@ TEST_RUNNER = 'hc.api.tests.CustomRunner'
 
 # You can switch database engine to postgres or mysql using environment
 # variable 'DB'. Travis CI does this.
-if os.environ.get("DB") == "postgres":
-    DATABASES = {
-        'default': {
-            'ENGINE':   'django.db.backends.postgresql',
-            'NAME':     'hc',
-            'USER':     'postgres',
-            'TEST': {'CHARSET': 'UTF8'}
-        }
+# if os.environ.get("DB") == "postgres":
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.db.backends.postgresql',
+        'NAME':     'hc',
+        'USER':     'postgres',
+        'TEST': {'CHARSET': 'UTF8'}
     }
+}
 
 if os.environ.get("DB") == "mysql":
     DATABASES = {
