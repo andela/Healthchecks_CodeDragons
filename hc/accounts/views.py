815a0ac40f4ad782950d594e3186089c825a31d4
jonathankamau <jonathan.kamau@andela.com>
2017-04-05 08:48:52 +0300

    removed handle import

--- hc/accounts/views.py
+++ hc/accounts/views.py
@@ -176,8 +176,7 @@ def profile(request):
                     profile.next_report_date = now + timedelta(days=30)
                     profile.save()
                 messages.success(request, "Your settings have been updated!")
-                command_object = sendreports.Command()
-                command_object.handle(loop=True)
+                
         elif "invite_team_member" in request.POST:
             if not profile.team_access_allowed:
                 return HttpResponseForbidden()
