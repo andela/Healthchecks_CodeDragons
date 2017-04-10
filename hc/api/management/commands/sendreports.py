7d65400aaa2152707379863ebd31086c52f2ac38
jonathankamau <jonathan.kamau@andela.com>
2017-04-10 13:09:20 +0300

    changed method

--- hc/api/management/commands/sendreports.py
+++ hc/api/management/commands/sendreports.py
@@ -16,7 +16,6 @@ def num_pinged_checks(profile):
 
 class Command(BaseCommand):
     help = 'Send due monthly reports'
-    item = ''
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -35,6 +34,7 @@ class Command(BaseCommand):
         # get current time
 
         now = timezone.now()
+
         # get time stamp for day before to
         # ensure user only gets email at the earliest at
         # least 1 day after subscribing
@@ -43,28 +43,34 @@ class Command(BaseCommand):
             "weekly": 7,
             "monthly": 30
         }
+
+        sent_list = []
+
         for item in number_of_days.keys():
+
             period_before = now - timedelta(days=number_of_days[item])
             # initialise db query conditions for next report date
             report_due = Q(next_report_date__lt=now)
             report_not_scheduled = Q(next_report_date__isnull=True)
             # filter query results
             report_period = Profile.objects.filter(report_due | report_not_scheduled)
-            # get rows where report preference is daily
+            # get rows depending on report preference
             report_period = report_period.filter(reports_allowed=item)
             report_period = report_period.filter(user__date_joined__lt=period_before)
             sent = 0
-            # send email to all users who choose daily reports
+            # send email to all users who chose current report type
             for profile in report_period:
                 # send email only if there were checks for current user
                 if num_pinged_checks(profile) > 0:
-                    # terminal output for daily report
+                    # terminal output for current report type
                     report_tmpl = "Sending " + item + " report to %s"
                     self.stdout.write(report_tmpl % profile.user.email)
                     profile.send_email_report(number_of_days[item], item)
                     sent += 1
+            
+            sent_list.append(sent)       
 
-        return sent, item
+        return sent_list
 
     def handle(self, *args, **options):
         """
@@ -72,7 +78,8 @@ class Command(BaseCommand):
         """
         if not options["loop"]:
             # configure return statement based on report type
-            return "Sent %d %d reports" % self.handle_one_run()
+            return "Sent %d daily reports %d weekly reports and %d monthly reports" \
+                   % self.handle_one_run()[0], self.handle_one_run()[1], self.handle_one_run()[2]
 
         self.stdout.write("sendreports is now running")
         while True:
